b5fbb0ec72a0ca833ff36c44bbbc59c37d9b9c4e
Andy Le <4ndyle@gmail.com>
2024-12-23 18:16:10 -0700

    create movement for player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
@@ -11,14 +12,31 @@ def main():
   # set a new GUI window
   screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
   on = True
+
+  # create groups for player
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable,drawable)
+
+  # instantiate player object
+  player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
   
   while on:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
-
+      
     screen.fill("black")
+
+    # calls on player objects that are updatable / drawable 
+    for sprite in updatable:
+      sprite.update(dt)
+    for sprite in drawable:
+      sprite.draw(screen)
+
     pygame.display.flip()
+
+    # limit frames to 60 fps
     dt = clock.tick(60) / 1000
 
 
